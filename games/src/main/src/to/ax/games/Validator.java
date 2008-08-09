6304009803ddb6212c0160265d6e32ccb82e622d
tom.ritchford <tom.ritchford@726d96a8-2351-0410-95be-91815ced7400>
2008-08-09 21:12:49 +0000

    Oops, miscreated this.

--- games/src/main/src/to/ax/games/Validator.java
+++
@@ -1,7 +0,0 @@
-package to.ax.games;
-
-public interface Validator<Game, Move> {
-  String whyIsGameInvalid(Game game);
-  String whyIsMoveInvalid(Move move);
-  String whyIsGameMoveInvalid(Game game, Move move);
-}
